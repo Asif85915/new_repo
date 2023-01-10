f0f8f3fa5bbb413ffc6a3514414f22d31b3a5d34
Asif <asif.sayyed@freestoneinfotech.com>
2023-01-10 17:51:14 +0530

    t1 commit

--- t1.py
+++ t1.py
@@ -7,3 +7,5 @@ sum = num1 + num2
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print("successful program")
+print("retest successful program")
+print("retest successful program")
